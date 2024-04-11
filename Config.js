f4174276b199c555901280129c5fe43564eea0f2
RATBAT1234 <166750351+RATBAT1234@users.noreply.github.com>
2024-04-11 13:35:25 -0500

    Update Config.js

--- Config.js
+++ Config.js
@@ -2,9 +2,9 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 //contact details
-global.ownernumber = ['919931122319']
+global.ownernumber = ['18762251976']
 global.ownername = "💙𓆩‎✘AʏᴜSʜ✘𓆪_✰"//owner name
-global.ytname = "YT: AyushBotz.inc"
+global.ytname = "YT: Professer"
 global.socialm = "GitHub: Ayushpandey023"
 global.location = "Indian"
 
